70a7611d24e0daade39c02e35464e6e07fd69c9d
CHEN Han <33325023+Concyclics@users.noreply.github.com>
2026-02-11 18:00:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 # LLM settings (OpenAI-compatible)
-export OPENAI_API_KEY="sk-e5b5507826934f9cada58fc49385050a"
-#export OPENAI_API_KEY="sk-0b5f4e4e4f9c4b1a9d3f8e2c3a4b5c6d"
+export OPENAI_API_KEY="YOU_API_KEY"
 export OPENAI_BASE_URL="https://api.deepseek.com"   # optional if using OpenAI default
 #export OPENAI_BASE_URL="https://api.moonshot.cn"   # optional if using OpenAI default
 export LUCID_ANALYZER_MODEL="deepseek-reasoner"
